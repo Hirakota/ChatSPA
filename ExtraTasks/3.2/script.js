74b99c82d8da3e8cb16e1708283c02d0cbf54bd3
Hirakota <askodmail@gmail.com>
2020-11-01 18:30:09 +0300

    Добавил текс в 3.2

--- ExtraTasks/3.2/script.js
+++ ExtraTasks/3.2/script.js
@@ -1,24 +1,37 @@
-let prices = [7,1,5,3,6,4];
+let prices = [7, 1, 5, 3, 6, 4];
+console.log("Task 1 Profit = " + maxProfit(prices));
+prices = [1, 2, 3, 4, 5];
+debugger;
+console.log("Task 2 Profit = " + maxProfit(prices));
+prices = [7, 6, 4, 3, 1];
+console.log("Task 3 Profit = " + maxProfit(prices));
 
+function maxProfit(price) {
+    debugger;
+    let n = price.length;
+    // Создать массив прибыли и инициализировать его как 0
 
-let cashProfile = {
-    maxCash: 0,
+    let profit = prices.slice(0).fill(0);
 
-    curentCash: 0,
-    isShares: false,
+    let maxPrice = price[n - 1];
 
-    buyShares: function(price) {
-        if(!this.isShares) {
-            this.curentCash -= price;
-            this.isShares = true;
-        }
-        
-    },
-    soldShares: function(price) {
-        if(this.isShares) {
-            this.curentCash += price;
-            this.isShares = false;
-        }
-    },
+    for (let i = n - 2; i >= 0; i--) {
+        if (price[i] > maxPrice) maxPrice = price[i];
+
+        profit[i] = Math.max(profit[i + 1], maxPrice - price[i]);
+    }
+
+    let minPrice = price[0];
+
+    for (let i = 1; i < n; i++) {
+        // min_price - минимальная цена в цене [0..i]
+
+        if (price[i] < minPrice) minPrice = price[i];
+
+        profit[i] = Math.max(profit[i - 1], profit[i] + (price[i] - minPrice));
+    }
+
+    let result = profit[n - 1];
+
+    return result;
 }
-
